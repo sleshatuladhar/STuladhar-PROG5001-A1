aaf5f8825628430ae8c2680203b3f94bd3f6bfa0
sleshatuladhar <sleshatuladhar@gmail.com>
2023-08-30 16:00:34 +1000

    Create classes assignment and main; get user input for assignment name

---
+++ Assignment.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/**
+ * Assignment class.
+ *
+ * @author (Slesha Tuladhar)
+ * @version (30/08/2023)
+ */
+
+public class Assignment
+{
+    // instance variables - replace the example below with your own
+    private String name;
+
+    /**
+     * Constructor for objects of class Assignment
+     */
+    public Assignment()
+    {
+    }
+
+    /**
+     * Input assignment name
+     */
+    public void inputAssignmentName()
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter assignment name: ");
+        this.name = input.nextLine();
+
+        input.close();
+    }
+}
---
+++ Main.java
@@ -0,0 +1,26 @@
+
+/**
+ * Main class.
+ *
+ * @author (Slesha Tuladhar)
+ * @version (30/08/2023)
+ */
+
+public class Main
+{
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+    }
+
+    /**
+     * Main method
+     */
+    public static void main(String[] args)
+    {
+        Assignment assignment = new Assignment();
+        assignment.inputAssignmentName();
+    }
+}
